a22490605a35766dd16bfac6147dd8b790da328f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-24 12:53:22 -0300

    Acabou

---
+++ Listas/Desafio.js
@@ -0,0 +1,11 @@
+let listaGenerica = []
+
+
+
+let listaLinguagens = ["Java","Ruby","Golang"]
+
+console.log(listaLinguagens[0])
+console.log(listaLinguagens[1])
+console.log(listaLinguagens[listaLinguagens.length-1])
+
+
---
+++ Listas/script.js
@@ -0,0 +1,14 @@
+let lista_Sorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarnumero();
+
+
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNalista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+        }
+    }
